3a31d550bb2db94f8bb7bf514acaaeb6ef90441f
Elijah Gutierrez <elijah@Elijahs-MacBook-Air.local>
2021-08-15 13:10:59 +0100

    Added copy and setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+HOME=/Users/elijah/Code/DissCode/LMEDS/
+FIGS=/Users/elijah/Code/DissCode/Plots/
+TESTS=${HOME}tests/
+USERSCR=${HOME}lmeds/user_scripts/
+PILOT=${HOME}tests/pilot_experiment/
+AUDIO=${HOME}tests/pilot_experiment/audio_and_video/
+PROSODY=${HOME}tests/prosody_experiment/
