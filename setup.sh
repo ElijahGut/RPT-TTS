db9ee60ad74fd581c9bf50e68c79d59b9defbf1e
ElijahGut <s1740779@ed.ac.uk>
2021-09-15 22:21:50 +0100

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-HOME=/Users/elijah/Code/DissCode/RPT-TTS/ # change this
-FIGS=/Users/elijah/Code/DissCode/Plots/ # change this
-EXP=${HOME}tests/rpt_tts_demo/ # change this
+HOME=/Users/elijah/Code/DissCode/RPT-TTS/ # change this to root directory 
 TESTS=${HOME}tests/
 USERSCR=${HOME}lmeds/user_scripts/
+# FIGS=/Users/elijah/Code/DissCode/Plots/ # change this to plot directory (optional)
