abd8774278d3145690b0c4ef977b8ca6a869d150
Elijah Gutierrez <elijah@Elijahs-MacBook-Air.local>
2021-09-17 12:25:15 +0100

    modified setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOME=/Users/elijah/Code/DissCode/RPT-TTS/ # change this to root directory 
-TESTS=${HOME}tests/
-USERSCR=${HOME}lmeds/user_scripts/
+export HOME=/Users/elijah/Code/DissCode/RPT-TTS/ # change this to root directory
+export TESTS=${HOME}tests/
+export USERSCR=${HOME}lmeds/user_scripts/
 # FIGS=/Users/elijah/Code/DissCode/Plots/ # change this to plot directory (optional)
